615339fe0f5d5a3bf84d76878608d12fff264c91
314lipczuk <3.14lipczuk@gmail.com>
2023-11-18 23:20:52 +0100

    speciation & offspring calculations

--- XOR.py
+++ XOR.py
@@ -4,6 +4,16 @@ import random
 import networkx as nx
 from pyvis.network import Network as VisualNetwork
 
+"""
+done
+- speciation
+
+todos 
+- mutation
+- crossover
+- toplevel loop
+"""
+
 class Node:
     next_node_id=0
 
@@ -61,6 +71,9 @@ class Genome:
                 self.connections.append(Connection(innov_id=Connection.get_innov_id(), in_node=i.node_id, out_node=o.node_id, weight=random.random()))
         self.refresh_layers()
     
+    def mutate(self):
+        pass
+    
     def refresh_layers(self):
         inputs = [n.node_id for n in self.nodes if n.node_type == 'input']
         def find_max_len_to_input(node_id):
@@ -117,30 +130,92 @@ class Genome:
 
         weight_diff = sum([ abs(a[i]-b[i]) for i in range(len(a))]) / len(a)
         return Genome.C1 * excess_count + Genome.C2 * disjoint_count + Genome.C3 * weight_diff
+    
+    def calculate_fitness(self, inputs, expected_outputs):
+        results = []
+        for i, o in zip(inputs, expected_outputs):
+            self.load_inputs(i)
+            self.run_network()
+            results.append((o, self.get_output()[0]))
+        error = sum( [abs( r - o ) for o, r in results] )
+        self.fitness = (4 - error) ** 2
 
 
 class Population:
     size = 50
+    specie_goal = 4
+
+    def calculate_offspring(self):
+        for s in self.species:
+            s.calculate_stats()
+        total_average = sum([s.average for s in self.species])
+        for s in self.species:
+            s.offspring = math.floor((s.average / total_average) * s.n)
+
     def __init__(self, size):
         self.organisms = []
         self.species = []
+        g = Genome(2,1)
+        for _ in range(size+1):
+            self.organisms.append(copy.deepcopy(g))
+        self.speciate()
+
+    def speciate(self):
+        for s in self.species:
+            s.clear_members()
+
+        for g in self.organisms:
+            added = False
+            for s in self.species:
+                cd = g.compatibility_distance(s.representative)
+                if cd < Specie.threshold:
+                    s.add_organism(g)
+                    added = True
+                    break
+            if not added:
+                self.species.append(Specie(id=len(self.species), representative=g))
+        self.species = [spec for spec in self.species if len(spec.organisms) > 0]
+        for s in self.species: 
+            s.representative = random.choice(s.organisms)
 
 class Specie:
     threshold = 3
-    def __init__(self, id):
+    use_adjusted_fitness=True
+    def __init__(self, id, representative):
         self.id = id
         self.organisms = []
-        self.best_organism = None
-        self.best_fitness = 0
-        self.average_fitness = 0
-        self.staleness = 0
-    
+        self.representative =  representative
 
-g = Genome(2,1)
-print(g.nodes)
-g.show()
-g.load_inputs([0,1])
-g.run_network()
-print([n.sum_output for n in g.nodes if n.node_type == 'output'])
-g1 = copy.deepcopy(g)
-print('compd dist', g.compatibility_distance(g1))
+    def add_organism(self, organism):
+        self.organisms.append(organism)
+
+    def calculate_adjusted_fitness(self):
+        N = len(self.organisms)
+        for o in self.organisms:
+            o.adjusted_fitness = o.fitness / N
+
+    def clear_members(self):
+        self.organisms = []
+    
+    def calculate_stats(self):
+        if Specie.use_adjusted_fitness:
+            self.calculate_adjusted_fitness()
+            self.average = sum([o.adjusted_fitness for o in self.organisms]) / len(self.organisms)
+        else:
+            self.average = sum([o.fitness for o in self.organisms]) / len(self.organisms)
+        self.n = len(self.organisms)
+
+#g = Genome(2,1)
+#print(g.nodes)
+#g.show()
+#g.load_inputs([0,1])
+#g.run_network()
+#print([n.sum_output for n in g.nodes if n.node_type == 'output'])
+#g1 = copy.deepcopy(g)
+#print('compd dist', g.compatibility_distance(g1))
+#INPUT = [[0,0], [0,1], [1,0], [1,1]]
+#OUTPUT = [0, 1, 1, 0]
+#g.calculate_fitness(INPUT, OUTPUT)
+#print('fitness', g.fitness)
+p = Population(50)
+print('species',len(p.species), p.species, len(p.species[0].organisms))
