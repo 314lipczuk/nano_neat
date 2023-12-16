321493585caa72fa53c00c941370df81bd5b682b
314lipczuk <3.14lipczuk@gmail.com>
2023-12-16 13:08:41 +0100

    walker quality of life changes

--- walker.py
+++ walker.py
@@ -1,4 +1,5 @@
 from model import Population, Config, Genome , Specie
+import time
 import gymnasium as gym
 import math
 import dill
@@ -40,32 +41,7 @@ def calculate_fitness(self, visible=False):
         fin_reward += reward
         if terminated or truncated:
             done = True
-    if truncated:
-        for i in range(3):
-            observation,info = environment.reset()
-            done = False       
-            fin_reward = 0
-            not_moving_counter = 0
-            while not done:
-                self.load_inputs(observation)
-                self.run_network()
-                output = self.get_output()
-                assert(len(output) == 4)
-                action = output
-                observation, reward, terminated, truncated, info = environment.step(action)
-                if reward < 0.01:
-                    not_moving_counter += 1
-                    if not_moving_counter > 30:
-                        done = True
-                else:
-                    not_moving_counter = 0
-                if terminated or truncated:
-                    done = True
-            if not truncated:
-                break
-        return (True, fin_reward)
-    #self.fitness = fin_reward
-    # finished, fitness
+    fin_reward = max(fin_reward, -100) + 101
     return (False, fin_reward)
 
 def every_generation(p:Population):
@@ -83,6 +59,14 @@ def after_finished(p:Population):
     champ = p.champion
     champ.calculate_fitness(visible=True)
 
+def print_generation(p:Population):
+    mean = sum([o.fitness for o in p.organisms]) / len(p.organisms)
+    std_dev = math.sqrt(sum([(o.fitness - mean) ** 2 for o in p.organisms]) / len(p.organisms))
+    n = time.clock_gettime_ns(time.CLOCK_MONOTONIC)
+    itertime = (n- p.t)/1000000000
+    p.t = n
+    print("Generation", p.generation, "\tfitness:", "%.2f" %(p.organisms[0].fitness - 100), "\tmean:", "%.2f" % (mean - 100), "\tstd dev:", "%.2f" %std_dev, "\tSpecies:", len(p.species), "Population ",len(p.organisms), "itertime:" ,round(itertime, 2), "s")
+
 DefaultWalkerConfig = Config(
     activation=activation_function,
     fitness=calculate_fitness, 
@@ -92,6 +76,10 @@ DefaultWalkerConfig = Config(
     meta = {"gym_environment": "BipedalWalker-v3", "experiment_name": "BIPEDAL" },
     every_generation=every_generation,
     after_finished=after_finished,
+    DYNAMIC_THRESHOLD = True,
+    problem_fitness_threshold = 390,
+    CROSS_SPECIE_REPRODUCTION =True,
+    print_generation = print_generation,
     )
 
 def main():
