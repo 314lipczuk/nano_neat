8b8d191beed5a18e39e9cc4b995589a7b22a3e0a
314lipczuk <3.14lipczuk@gmail.com>
2023-12-24 17:47:44 +0100

    xor 2

---
+++ xor2.py
@@ -0,0 +1,63 @@
+from model import Population, Config, Genome , Specie
+import gymnasium as gym
+import math
+import dill
+import pickle
+import torch
+
+def activation_function(self, x):
+    x = torch.tensor(x)
+    if x >= 0:
+        return float(1./(1+torch.exp(-1e5*x)).to(torch.float))
+    else:
+        return float(torch.exp(1e5*x)/(1+torch.exp(1e5*x)).to(torch.float))
+
+def calculate_fitness(self, visible=False):
+    INPUT = [[0,0,1], [0,1,1], [1,0,1], [1,1,1]]
+    OUTPUT = [0, 1, 1, 0]
+    results = []
+    for i, o in zip(INPUT,OUTPUT):
+        self.load_inputs(i)
+        self.run_network()
+        output = self.get_output()[0]
+        results.append((o, output ))
+    error = sum( [abs( r - o ) for o, r in results] )
+    fitness = round((4 - error) ** 2, 2)
+    isDone = fitness > 15.5
+    return (isDone, fitness)
+
+def every_generation(p:Population):
+    pass
+    #if p.generation % 10 == 0 and p.generation > 0:
+    #        name = f"gen_{p.generation}"
+    #        f = open(f"{p.path}/{name}.pkl", "wb")
+    #        dill.dump(p.organisms[0], f )
+    #        f.close()
+    #        print(f"Saved model {p.serial_number} {name}")
+
+def after_finished(p:Population):
+    champ = p.champion
+    #champ.calculate_fitness(visible=True)
+    champStats = f"{p.generation} {len(champ.nodes)} {len(champ.connections)} "
+    with open(f"{p.path}/champStats.txt", "w") as f:
+        f.write(champStats)
+
+XorConfig = Config(
+    activation=activation_function,
+    fitness=calculate_fitness, 
+    input_size=3, 
+    EXPERIMENT_PATH= './tmp/XOR2',
+    output_size=1 ,
+    meta = {},
+    every_generation=every_generation,
+    after_finished=after_finished,
+    max_iterations=300,
+    )
+
+def main():
+    p = Population(config=XorConfig)
+    for o in p.organisms: o.connections[-1].weight = 1
+    p.run()
+
+if __name__ == "__main__":
+    main()
